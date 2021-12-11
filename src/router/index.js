191862a96a22422a36f7881626306a7829fbdd68
muchuan <mu.chuan.hung@gmail.com>
2021-12-12 02:36:47 +0800

    feat: 路由分頁設定

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,65 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
 
 Vue.use(VueRouter)
 
 const routes = [
+  //component動態載入
+  //設定根目錄-5轉址
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'root',
+    redirect: '/homepage'
   },
+  //首頁
+  {
+    path: '/homepage',
+    name: 'homepage',
+    component: () => import('../views/HomePage.vue')
+  },
+  //商品頁面
+  {
+    path: '/product/:id',
+    name: 'product',
+    component: () => import('../views/Product.vue')
+  },
+   //所有商品
+  {
+    path: '/products',
+    name: 'products',
+    component: () => import('../views/Products.vue')
+  },
+  //結帳頁面
+  {
+    path: '/checkout',
+    name: 'checkout',
+    component: () => import('../views/Checkout.vue')
+  },
+  //分類頁
+  {
+    path: '/category',
+    name: 'category',
+    component: () => import('../views/Category.vue')
+  },
+  //搜尋
+  {
+    path: '/search',
+    name: 'search',
+    component: () => import('../views/Search.vue')
+  },
+  //簡介
   {
     path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    name: 'about',
+    component: () => import('../views/About.vue')
+  },
+  //搜索不到頁面
+  {
+  //星號 (*) 是萬用字元，這裡代表「所有的網址」
+    path: '*',
+    name: 'not-found',
+    component: () => import('../views/PageNotFound.vue')
   }
 ]
 
@@ -24,4 +67,8 @@ const router = new VueRouter({
   routes
 })
 
+
 export default router
+
+     
+   
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,5 @@
 <template>
   <h1>
-    404 Page Not Found
+   homepage
   </h1>
 </template>
