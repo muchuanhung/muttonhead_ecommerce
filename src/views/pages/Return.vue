e2043498ad2d38bb1f1dcbaad0500f78794f8f00
muchuan <mu.chuan.hung@gmail.com>
2021-12-18 23:21:40 +0800

    refractor: single page update

---
+++ src/views/pages/Return.vue
@@ -0,0 +1,194 @@
+<template>
+    <div class="return-wrapper">
+			<h4 class="return-wrapper__title">RETURNS // EXCHANGES// WARRANTIES</h4>
+      <div class="return-wrapper__content">
+        <div class="return-wrapper__content__section01">
+        <p><strong>RETURNS & EXCHANGES</strong></p>
+        <p  style="font-weight:bold;"> *** PLEASE NOTE OUR RETURN ADDRESS HAS CHANGED. </p>
+        <p  style="font-weight:bold;"> Our New Address is:</p>
+        <p  style="font-weight:bold;"> 163 Roncesvalles Ave. Toronto, Ontario, M6R2L3 </p>
+        </div>
+
+        <div class="return-wrapper__content__section02">
+        <p style="font-weight:bold;"> *contact us via the shipping email within 7 days of receiving your item(s), as long as we receive an email from you in this time period we are happy to honour the exchange/return period until your package gets back to us* </p>
+        </div>
+
+        <div class="return-wrapper__content__sub-title">
+        <p><strong> IN STORE REGULAR PRICED MERCHANDISE </strong></p>
+        </div>
+        <ul>
+        <li> &middot; Within 15 days of purchase, you can return your item for a full refund onto the original form of payment, which must be present during return.</li>
+        <li> &middot; Within 30 days of purchase, you can exchange your item or alternatively we can provide you with a store credit.</li>
+        <li> &middot; After 30 days, all sales are final.</li>
+        </ul>
+
+         <div class="return-wrapper__content__sub-title"> 
+         <p><strong> IN STORE SALE OR ITEMS PURCHASED WITH A DISCOUNT CODE </strong></p>
+         </div>
+         <ul>
+          <li> &middot; Within 15 days of purchase, you can send your item back for a store credit. Store credit does not expire and can be used online or in-store and can be used to purchase a different size or style. </li>
+          <li> &middot; After 15 days, all sales are final.  </li>  
+         </ul>  
+
+          <p> To be eligible for a return, your item must be "<strong> unused </strong>" in the same condition that you received it, with the tags attached, and you must have the receipt. </p>
+          <p> Please note that the following items are final sale: all apothecary & grooming goods, socks, toques, underwear and masks. </p>
+          <p> Gift cards are not refundable. </p>
+          
+          <div class="return-wrapper__content__sub-title">
+          <p><strong> ONLINE REGULAR PRICED MERCHANDISE </strong></p>
+          </div>
+          <ul>
+          <li> &middot; EMAIL shipping@muttonheadstore.com </li>
+          <li> &middot; Within 15 days of delivery date, you can contact the shipping department to initiate a return of your item for a full refund onto the original form of payment or for a store credit if you choose </li>
+          <li> &middot; Within 30 days of delivery date, you can contact the shipping department to initiate an exchange your item or alternatively we can provide you with a store credit.</li>
+          <li> &middot; After this 30 days all sales are final. </li>
+          </ul> 
+
+          <div class="return-wrapper__content__sub-title">
+          <p><strong> ONLINE SALE OR ITEMS PURCHASED WITH A DISCOUNT CODE </strong></p>
+          </div>
+          <ul>
+          <li> &middot; EMAIL shipping@muttonheadstore.com </li>
+          <li> &middot; Within 15 days of delivery date, you can contact the shipping department to initiate a return and return your item for store credit. </li>
+          <li> &middot; After 15 days, the sale is final. </li>
+          </ul> 
+
+           <p> To be eligible for a return, your item must be "<strong> unused </strong>" and in the same condition, with tags attached, as you received it and you must have the receipt. </p>
+           <p> Please note that the following items are final sale: all apothecary & grooming goods, socks, toques, underwear and masks. </p>
+           <p> Gift cards are not refundable. </p>
+
+           <div class="return-wrapper__content__sub-title"> 
+           <p><strong> CURBSIDE PICKUP ORDERS THAT ARE NOT PICKED UP </strong></p>
+           </div>
+            <p> We are more than happy to hold your pickup order for as long as you need to pick it up. Just let us know if you need an extended period for what ever reason and we will gladly accommodate it for you! </p>
+            <p> However, we do have limited space to hold orders. If an order is not picked up within 60 days of the order being placed, we will send you email reminding you about the order. If you do not respond within 7 days, we will automatically cancel the order and send you store credit for the value of the order. </p>
+            <p> Please note, we do not issue refunds on orders that not picked up within 14 days. We will issue you a store credit if you no longer wish to pick up the order. </p>
+
+            <div class="return-wrapper__content__sub-title">
+            <p><strong> Refunds: </strong></p>
+            </div>
+            <p> Once your return is received and inspected, we will send you an email to notify you that we have received your returned item. We will also notify you of the approval or rejection of your refund. If you are approved, then your refund will be processed, and a credit will automatically be applied to your credit card or original method of payment, within  1-3 business days. </p>
+          
+
+            <div class="return-wrapper__content__sub-title">
+            <p><strong> Late or missing refunds: </strong></p>
+            </div>
+            <p> If you haven’t received a refund yet, please check your bank account again.
+            Then contact your credit card company, it may take some time before your refund is officially posted.Next contact your bank. There is often some processing time before a refund is posted.If you’ve done all of this and you still have not received your refund yet, please contact us atshipping@muttonheadstore.com </p>
+          
+
+            <div class="return-wrapper__content__sub-title">
+            <p><strong> Exchanges: </strong></p>
+            </div>
+            <p> If you ordered the wrong size or colour we can provide you with a store credit that can be used to purchase the alternate item. Please fill out the RETURN//EXCHANGE form that was included in your shipment and contact us at shipping@muttonheadstore.com to confirm the exchange. </p>
+
+            <div class="return-wrapper__content__section03">
+            <p> Send your return package back to: </p>
+            </div>
+
+        
+            <p> **NEW ADDRESS: </p>
+            <div class="return-wrapper__content__section04">
+            <strong>Muttonhead- RETURNS</strong>
+            <strong>163 Roncesvalles Ave.</strong>
+            <strong>Toronto  Ontario</strong>
+            <strong>CANADA</strong>
+            <strong>M6R 2L3</strong>
+            </div>
+
+             <div class="return-wrapper__content__section05">
+            <p> It is your responsibility to pay the shipping costs associated with returning your item. Shipping costs are non-refundable. We recommend shipping with "<strong>Canada Post </strong>" or "<strong> USPS </strong>" as these are often much cheaper than other shipping companies. </p>
+             </div>
+            <p> Once we receive your returned package we will issue you a store credit.  </p>
+
+            <p style="font-weight:bold;"> If you are shipping an item over $75, you should consider using a trackable shipping option or purchasing shipping insurance. We cannot guarantee that we will receive your returned item. Make sure to keep your tracking number. </p>
+
+            <div class="return-wrapper__content__sub-title">
+            <p><strong> PRICE ADJUSTMENTS: </strong></p>
+            </div>
+
+            <div class="return-wrapper__content__section06">
+              <p> Sale Adjustments: If a purchased item is marked down for a final markdown or end of season sale, Muttonhead offers a one-time price adjustment on your order under the following conditions: </p>
+            </div>  
+              <ul>
+                <li> &middot; Price adjustment request must be made within 7-days of your order being placed. </li> 
+                <li> &middot; The same style and size must be in stock at the time of your price adjustment request. </li> 
+                <li> &middot; The price difference must be refunded to gift card </li> 
+              </ul>
+
+            <div class="return-wrapper__content__section07">
+              <p> Promotional Adjustments: If a promotional discount is offered (this includes Black Friday and Boxing Day!) Muttonhead offers a one-time price adjustment on your order under the following conditions: </p>
+            </div>  
+              <ul>
+                <li> &middot; Price adjustment request must be made within 24-hours of your order being placed. </li> 
+                <li> &middot; The same style and size must be in stock at the time of your price adjustment request. </li> 
+                <li> &middot; The price difference must be refunded to gift card </li> 
+              </ul>
+      </div>  
+    </div> 
+</template>
+
+
+<style lang="scss" scoped>
+
+
+.return-wrapper {
+  margin: 5rem 1rem 2rem 1rem;
+  //處理溢版問題
+  overflow-x: hidden;
+  overflow-y: visible;
+  position: relative;
+  cursor: default;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  
+  &__title {
+  display: flex;
+  justify-content: center;
+  padding: 30px;
+	font-size: 2rem;
+	font-weight: 700;
+  }
+
+   &__content{
+   width: 768px;
+   display: flex;
+   justify-content: center;
+   flex-direction: column;
+   margin-left: 30%;
+   letter-spacing: 0.75px;
+   line-height: 150%;
+   &__sub-title{
+   padding-top: 30px; 
+   padding-bottom: 10px;  
+   }
+   &__section01{
+   line-height: 300%;
+   padding-bottom: 20px;  
+   }
+   &__section02{
+   padding-bottom: 20px;  
+   }
+   &__section03{
+   padding: 30px 0;  
+   }
+   &__section04{
+   padding-bottom: 20px;
+   display: flex;
+   flex-direction: column;  
+   }
+   
+   &__section05{
+   padding: 30px 0;  
+   }
+   &__section06{
+   padding-bottom: 20px;  
+   } 
+   &__section07{
+   padding: 30px 0;  
+   }
+  }
+
+}
+</style>
---
+++ src/views/pages/Shipping.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="contact-wrapper">
+			<h4 class="contact-wrapper__title">CONTACT US</h4>
+      <div class="contact-wrapper__content">
+        <p class="contact-wrapper__content-text">
+          <strong>General Inquiries:</strong>store@muttonheadstore.com</p>
+         <p class="contact-wrapper__content-text">
+          <strong>Online Orders:</strong>shipping@muttonheadstore.com</p>       
+       </div>  
+       <div class="contact-wrapper__content-address">
+          <strong>163 Roncesvalles Avenue, Toronto, ON.</strong>Phone number: 647-812-2700
+       </div> 
+    </div> 
+</template>
+
+
+<style lang="scss" scoped>
+.contact-wrapper {
+  margin: 5rem 1rem 2rem 1rem;
+  //處理溢版問題
+  overflow-x: hidden;
+  overflow-y: visible;
+  position: relative;
+  cursor: default;
+  
+  &__title {
+  display: flex;
+  justify-content: center;
+  padding: 30px;
+	font-size: 2rem;
+	font-weight: 700;
+  }
+
+   &__content{
+   flex-direction: column;
+   text-align: center;
+   letter-spacing: 0.75px;
+   line-height: 200%;
+   padding: 20px 0;
+  }
+   &__content-address{
+   display: flex;
+   letter-spacing: 0.75px;
+   line-height: 150%;
+   text-align: center;
+   flex-direction: column;
+  }
+}
+</style>
